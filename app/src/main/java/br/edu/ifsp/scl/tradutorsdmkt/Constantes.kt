83280f427cb8883215f32246c5fbde42e3000070
hartemav <victor.harteman@hotmail.com>
2019-07-03 00:09:30 -0300

    Commit 1

---
+++ app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.tradutorsdmkt
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v2/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "fb70eb7e" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "5f172c9371f01d6caa7ecc508cf6a86d" // Preeencher com seu app_key
+}
